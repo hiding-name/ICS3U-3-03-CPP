69d118d09bb55a94dbada03251583afc1516236d
Ubuntu <ubuntu@ip-172-31-18-91.ec2.internal>
2019-10-02 21:51:39 +0000

    Initial code

---
+++ guessNumber.cpp
@@ -0,0 +1,23 @@
+// Copyright (c) 2019 Matsuru All rights reserved.
+//
+// Created by: Matsuru Hoshi
+// Created on: Sept 2019
+// This is program number guesser
+
+#include <iostream>
+
+int main() {
+    // This will add numbers
+    const number;
+    int guess;
+
+    // input
+    std::cout << "\nGuess the number: " << std::endl;
+    std::cin >> guess;
+
+    // process
+    if (guess == number) {
+        // output
+        std::cout << "You got it.";
+    }
+}
